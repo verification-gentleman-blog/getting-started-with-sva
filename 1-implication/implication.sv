66dd142c00c0b73d4308423e036a850267da347e
Tudor Timi <tudortimi@users.noreply.github.com>
2020-04-01 19:40:47 +0200

    Add code for overlapping implication
    Can't generate 'cover' traces.

---
+++ 1-implication/implication.sv
@@ -0,0 +1,24 @@
+module implication(input bit clk);
+
+   bit antecedent;
+   bit consequent;
+
+   
+   default clocking @(posedge clk);
+   endclocking
+
+
+   property overlapping_implication;
+     antecedent |-> consequent;
+   endproperty
+
+
+   // FIXME Can't get this to cover
+   cover_overlapping_implication: cover property (overlapping_implication);
+
+   // TODO Remove once 'cover' generates trace
+   // Forces a fail of the implcation property to generate a trace
+   assign consequent = 0;
+   assert_overlapping_implication: assert property (overlapping_implication);
+   
+endmodule
