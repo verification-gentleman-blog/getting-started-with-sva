e92993052390aa48d9d3da48c4c8b84824487b4a
Tudor Timi <tudortimi@users.noreply.github.com>
2020-04-04 11:00:47 +0200

    Fix trace to use 'sequence.triggered'

--- 5-sequence_triggered/sequence_triggered.sv
+++ 5-sequence_triggered/sequence_triggered.sv
@@ -12,23 +12,20 @@ module sequence_triggered(input bit clk);
   // Signals used in sequence have to be arguments, otherwise the tool will
   // complain.
   sequence accepted_request(bit request, bit accept, bit cancel);
+    @(posedge clk)
     request ##1 (!cancel throughout accept [->1]);
   endsequence
 
 
   bit busy;
   
-
   // Causes a segmentation fault
   //become_busy_only_due_to_accepted_request: assert property (
   //    $rose(busy) |-> accepted_request.triggered);
 
-  // Doesn't work as expected
-  //become_busy_only_due_to_accepted_request_trace: cover property (
-  //    $rose(busy) && accepted_request.triggered);
 
   become_busy_only_due_to_accepted_request_trace: cover property (
-      accepted_request(request, accept, cancel) ##0 $rose(busy));
+      $rose(busy) && accepted_request(request, accept, cancel).triggered);
 
   
   // Makes the traces more interesting
