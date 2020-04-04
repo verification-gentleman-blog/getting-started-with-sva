851fe5208facb0aee10e5e0c21078dc7450aef3d
Tudor Timi <tudortimi@users.noreply.github.com>
2020-04-04 10:16:46 +0200

    Add named sequence

--- 4-named_sequences/named_properties_and_sequences.sv
+++ 4-named_sequences/named_properties_and_sequences.sv
@@ -19,5 +19,23 @@ module named_properties_and_sequences(input bit clk);
 
   on_group1: assert property (
     some_property(a1, b1, c1, d1));
+
+
+
+  sequence antecedent(bit a, bit b, bit c);
+    a ##1 b [*2] ##1 c [*3];
+  endsequence
+
+
+  bit a2, b2, c2, d2, e2, f2, g2;
+
+  on_group2_d: assert property (
+    antecedent(a2, b2, c2) |-> d2);
+
+  on_group2_not_e: assert property (
+    antecedent(a2, b2, c2) |-> !e2);
+
+  on_group2_f_followed_by_g: assert property (
+    antecedent(a2, b2, c2) |-> f2 ##1 g2);
   
 endmodule
