ceff60bf0d1aa67dbcf0e57579258118e83595b3
Tudor Timi <tudortimi@users.noreply.github.com>
2020-04-04 15:48:20 +0200

    Add examples of delay operator in implication property

--- 2-delay_operator/delay_operator.sv
+++ 2-delay_operator/delay_operator.sv
@@ -6,12 +6,22 @@ module delay_operator(input bit clk);
 
   bit a;
   bit b;
-  
+
 
   a_followed_by_b_trace: cover property (
       a ##1 b);
 
   a_followed_by_b_after_3_cycles_trace: cover property (
       a ##3 b);
-    
+
+
+  bit c;
+
+
+  delay_in_antecedent: assert property (
+      a ##1 b |-> c);
+
+  delay_in_consequent: assert property (
+      c |-> a ##1 b);
+
 endmodule
