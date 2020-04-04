5172da68e18ce14c4579a4d0ca86aa18223d3f00
Tudor Timi <tudortimi@users.noreply.github.com>
2020-04-04 16:26:42 +0200

    Remove immediate code from 'repetition'

--- 3-repetition/repetition.sv
+++ 3-repetition/repetition.sv
@@ -6,37 +6,6 @@ module repetition(input bit clk);
 
    bit a;
    
-
-   sequence five_times_a;
-      a [*5];
-   endsequence
-
-   cover_five_times_a: cover property (five_times_a);
-
-
-   int unsigned counter;
-
-   always @(posedge clk)
-     if (a && counter < 5-1)
-       counter++;
-     else
-       counter = 0;
-
-   always @(posedge clk)
-     cover (counter == 5-1 && a);
-
-
-   // Needed to initialize counter
-   bit init = 1;
-
-   always @(posedge clk) begin
-     if (init)
-       assume (counter == 0);
-     init <= 0;
-   end
-
-
-  // TODO Add section about goto repetition?
-  // It might take a bit too long to explain.
+   cover_five_times_a: cover property (a [*5]);
    
 endmodule
