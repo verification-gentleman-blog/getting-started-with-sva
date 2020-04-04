3930acaacad18d729721226fd5ff28472f150f05
Tudor Timi <tudortimi@users.noreply.github.com>
2020-04-04 16:03:31 +0200

    Fix indentation

--- 3-repetition/repeat_five_times.sv
+++ 3-repetition/repeat_five_times.sv
@@ -1,27 +1,28 @@
 module repeat_five_times(input bit clk);
 
-   bit a;
+  bit a;
    
 
-   int unsigned counter;
-
-   always @(posedge clk)
-     if (a && counter < 5-1)
-       counter++;
-     else
-       counter = 0;
-
-   always @(posedge clk)
-     cover (counter == 5-1 && a);
+  int unsigned counter;
 
 
-   // Needed to initialize counter
-   bit init = 1;
+  always @(posedge clk)
+    if (a && counter < 5-1)
+      counter++;
+    else
+      counter = 0;
 
-   always @(posedge clk) begin
-     if (init)
-       assume (counter == 0);
-     init <= 0;
-   end
+  always @(posedge clk)
+    cover (counter == 5-1 && a);
+
+
+  // Needed to initialize counter
+  bit init = 1;
+
+  always @(posedge clk) begin
+    if (init)
+      assume (counter == 0);
+    init <= 0;
+  end
    
 endmodule
