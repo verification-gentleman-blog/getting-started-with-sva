69e249f52d66ea31f0ed3baeb66adf0fc28b359c
Tudor Timi <tudortimi@users.noreply.github.com>
2020-04-04 11:54:00 +0200

    Add concurrent version of delay operator

---
+++ 2-delay_operator/delay_operator.sv
@@ -0,0 +1,17 @@
+module delay_operator(input bit clk);
+
+  default clocking @(posedge clk);
+  endclocking
+
+
+  bit a;
+  bit b;
+  
+
+  a_followed_by_b_trace: cover property (
+      a ##1 b);
+
+  a_followed_by_b_after_3_cycles_trace: cover property (
+      a ##3 b);
+  
+endmodule
