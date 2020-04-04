3824311d37e9619de94d46f3b69c5952ddb5a487
Tudor Timi <tudortimi@users.noreply.github.com>
2020-04-04 16:01:46 +0200

    Move immediate example of repetition to own file

---
+++ 3-repetition/repeat_five_times.sv
@@ -0,0 +1,27 @@
+module repeat_five_times(input bit clk);
+
+   bit a;
+   
+
+   int unsigned counter;
+
+   always @(posedge clk)
+     if (a && counter < 5-1)
+       counter++;
+     else
+       counter = 0;
+
+   always @(posedge clk)
+     cover (counter == 5-1 && a);
+
+
+   // Needed to initialize counter
+   bit init = 1;
+
+   always @(posedge clk) begin
+     if (init)
+       assume (counter == 0);
+     init <= 0;
+   end
+   
+endmodule
