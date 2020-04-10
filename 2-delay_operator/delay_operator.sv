b92430f26523a08a72eabc88592b70b25f4309aa
Tudor Timi <tudortimi@users.noreply.github.com>
2020-04-10 20:15:47 +0200

    Move immediate version of single cycle delay to main module

--- 2-delay_operator/delay_operator.sv
+++ 2-delay_operator/delay_operator.sv
@@ -9,12 +9,54 @@ module delay_operator(input bit clk);
 
 
   parameter enum { SINGLE, MULTI } delay_kind = `DELAY_KIND;
+  parameter enum { CONCURRENT, IMMEDIATE } assert_kind = `ASSERT_KIND;
 
   if (delay_kind == SINGLE) begin: single
+    if (assert_kind == CONCURRENT) begin: concurrent
 
-    a_followed_by_b: cover property (
-        a ##1 b);
+      a_followed_by_b: cover property (
+          a ##1 b);
 
+    end
+    if (assert_kind == IMMEDIATE) begin: immediate
+
+      typedef enum { IDLE, A_SEEN, END } state_e;
+
+      state_e state;
+      state_e next_state;
+      bit match;
+
+      always_ff @(posedge clk)
+        state <= next_state;
+
+      always_comb begin
+        next_state = state;
+
+        case (state)
+          IDLE:
+            if (a)
+              next_state = A_SEEN;
+          A_SEEN:
+            next_state = END;
+        endcase
+      end
+
+      assign match = (state == A_SEEN) && b;
+
+
+      always @(posedge clk)
+        cover (match);
+
+
+      bit init = 1;
+
+      always @(posedge clk) begin
+        if (init)
+          assume (state == IDLE);
+        init <= 0;
+      end
+
+    end
   end
   if (delay_kind == MULTI) begin: multi
 
