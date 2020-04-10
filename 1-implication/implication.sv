b74771182b230ec0abeb82e3b51636b31d2b923a
Tudor Timi <tudortimi@users.noreply.github.com>
2020-04-10 17:37:27 +0200

    Conditionally add concurrent/immediate asserts based on parameter

--- 1-implication/implication.sv
+++ 1-implication/implication.sv
@@ -1,47 +1,74 @@
 module implication(input bit clk);
 
-   bit antecedent;
-   bit consequent;
-
-   
-   default clocking @(posedge clk);
-   endclocking
+  bit antecedent;
+  bit consequent;
 
 
-   property overlapping_implication;
-     antecedent |-> consequent;
-   endproperty
+  typedef enum {
+    CONCURRENT,
+    IMMEDIATE
+  } assert_kind_e;
+
+  // XXX WORKAROUND Can't select 'assert_kind' using '-chparam'. The tool complains that it
+  // can't parse an enum literal value. I tried using all sorts of quotes, but couldn't get
+  // it to work.
+  parameter assert_kind = `ASSERT_KIND;
 
 
-   // FIXME Can't get this to cover
-   cover_overlapping_implication: cover property (overlapping_implication);
+  if (assert_kind == CONCURRENT) begin
 
-   // TODO Remove once 'cover' generates trace
-   assert_overlapping_implication: assert property (overlapping_implication);
-
-   // Forces a fail of the implcation property to generate a trace
-   assign consequent = 0;
+    default clocking @(posedge clk);
+    endclocking
 
 
-   always @(posedge clk)
-     if (antecedent)
-       assert (consequent);
+    property overlapping_implication;
+      antecedent |-> consequent;
+    endproperty
 
 
-   property nonoverlapping_implication;
-     antecedent |=> consequent;
-   endproperty
+    // FIXME Can't get this to cover
+    cover_overlapping_implication: cover property (overlapping_implication);
+
+    // TODO Remove once 'cover' generates trace
+    assert_overlapping_implication: assert property (overlapping_implication);
+
+    // Forces a fail of the implcation property to generate a trace
+    assign consequent = 0;
+
+  end
 
 
-   // FIXME Can't get this to cover
-   cover_nonoverlapping_implication: cover property (nonoverlapping_implication);
+  if (assert_kind == IMMEDIATE) begin
 
-   // TODO Remove once 'cover' generates trace
-   assert_nonoverlapping_implication: assert property (nonoverlapping_implication);
+    always @(posedge clk)
+      if (antecedent)
+	assert (consequent);
+
+  end
 
 
-  always @(posedge clk)
-     if ($past(antecedent))
-       assert (consequent);
-   
+  if (assert_kind == CONCURRENT) begin
+
+    property nonoverlapping_implication;
+      antecedent |=> consequent;
+    endproperty
+
+
+    // FIXME Can't get this to cover
+    cover_nonoverlapping_implication: cover property (nonoverlapping_implication);
+
+    // TODO Remove once 'cover' generates trace
+    assert_nonoverlapping_implication: assert property (nonoverlapping_implication);
+
+  end
+
+
+  if (assert_kind == IMMEDIATE) begin
+
+    always @(posedge clk)
+      if ($past(antecedent))
+	assert (consequent);
+
+  end
+
 endmodule
