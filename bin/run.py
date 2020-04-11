1aa8fb773d1196a580a88f3fe32a161858bbb2c4
Tudor Timi <tudortimi@users.noreply.github.com>
2020-04-11 16:38:29 +0200

    Implement 'sby' command in run script

--- bin/run.py
+++ bin/run.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
+import pathlib
+import subprocess
+
 import click
 
-@click.group()
-@click.argument('task')
+sby_files = list(pathlib.Path.cwd().glob('*.sby'))
+assert len(sby_files) == 1, 'expected a single sby file in this directory'
+
+SBY_FILE = sby_files[0].name
+TASKS = subprocess.check_output(
+            ['sby', SBY_FILE, '--dumptasks'],
+            universal_newlines=True).split()
+
+@click.group(context_settings=dict(help_option_names=['-h', '--help']))
+@click.option('--task', '-t', required=True, type=click.Choice(TASKS))
 @click.pass_context
 def cli(ctx, task):
     ctx.ensure_object(dict)
@@ -13,6 +24,7 @@ def cli(ctx, task):
 @click.pass_context
 def sby(ctx):
     click.echo('Running sby with task {}'.format(ctx.obj['task']))
+    subprocess.call(['sby', '-f', SBY_FILE, ctx.obj['task']])
 
 @cli.command()
 @click.pass_context
