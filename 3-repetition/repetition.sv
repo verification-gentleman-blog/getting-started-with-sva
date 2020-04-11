493f3a0620e57858113044c1c3a6672248b712d0
Tudor Timi <tudortimi@users.noreply.github.com>
2020-04-11 11:24:58 +0200

    Group code concurrent/immediate repetition cover to same file

--- 3-repetition/repetition.sv
+++ 3-repetition/repetition.sv
@@ -1,11 +1,42 @@
 module repetition(input bit clk);
 
-   default clocking @(posedge clk);
-   endclocking
+  bit a;
+
+  default clocking @(posedge clk);
+  endclocking
 
 
-   bit a;
-   
-   cover_five_times_a: cover property (a [*5]);
-   
+  parameter enum { CONCURRENT, IMMEDIATE } assert_kind = `ASSERT_KIND;
+
+  if (assert_kind == CONCURRENT) begin: concurrent
+
+    five_times_a: cover property (a [*5]);
+
+  end
+  if (assert_kind == IMMEDIATE) begin: immediate
+
+    int unsigned counter;
+
+    always_ff @(posedge clk)
+      if (a && counter < 5-1)
+        counter++;
+      else
+        counter = 0;
+
+    assign match = (counter == 5-1 && a);
+
+    always @(posedge clk)
+      cover (match);
+
+    // Needed to initialize counter
+    bit init = 1;
+
+    always @(posedge clk) begin
+      if (init)
+        assume (counter == 0);
+      init <= 0;
+    end
+
+  end
+
 endmodule
