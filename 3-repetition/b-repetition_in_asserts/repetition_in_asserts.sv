4635dad6bc85260dd617821cdac1db8a9d6f2db0
Tudor Timi <tudortimi@users.noreply.github.com>
2020-04-12 09:54:46 +0200

    Add example of consecutive repetition operator in an assert

--- 3-repetition/b-repetition_in_asserts/repetition_in_asserts.sv
+++ 3-repetition/b-repetition_in_asserts/repetition_in_asserts.sv
@@ -8,6 +8,15 @@ module repetition_in_asserts(input bit clk);
 
 
   if (repetition_kind == CONSECUTIVE) begin: consecutive
+
+    bit start;
+    bit busy;
+
+    assert property (start |=> busy [*5]);
+
+    // Make the traces more interesting
+    assume property (start |=> busy [*3]);
+
   end
   if (repetition_kind == GOTO) begin: goto
   end
